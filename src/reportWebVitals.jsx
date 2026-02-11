7c38266e12898f7205fe2227bddd273e6faa3fd1
Aradu92 <alradulescu@ucsd.edu>
2026-02-10 19:34:48 -0800

    attempted fix of performance bug

--- src/reportWebVitals.jsx
+++ src/reportWebVitals.jsx
@@ -1,13 +1,15 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+// This file is used to measure performance in the app. Due to lag it has been commented out.
 
-export default reportWebVitals;
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
